479bea7748b0d53e3b1a88567a8a9da36f5c3b4d
Abdullah Usman <iamabdullahusman837@gmail.com>
2024-01-03 11:44:35 +0500

    commit files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Abdullah!");
+
+string firstname = "Abdullah";
+string secondname = "usman";
+Console.WriteLine($"My first name is {firstname} and my second name is {secondname}");
