5cf9e46a13577b4e362dc10cddc7679964ad9506
Abdullah Usman <iamabdullahusman837@gmail.com>
2024-01-04 12:36:48 +0500

    Loops

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Abdullah!");
-// Numbers and integers 
-long a = 10000000000000000;
-int b = 20;
-long c = a + b;
-
-Console.WriteLine(c);
+﻿int counter = 10;
+do
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+while(counter < 5);
