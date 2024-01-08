5eedbd0e6d30162f46dc5d733879e7fed76645d5
Abdullah Usman <iamabdullahusman837@gmail.com>
2024-01-08 10:32:32 +0500

    Sorting and Searching Lists

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,9 @@
-﻿var names = new List<string> {"Abdullah", "Ahsan" , " Bilal"};
+﻿var numbers = new List <int> {45,56,99,48,67,78};
 
-names.Add("Abdullah");
-names.Add("Ahsan");
-names.Add("Bilal");
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)} ");
+numbers.Sort();
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)} ");
 
-foreach(var name in names)
-{
-    Console.WriteLine("$Hello {name.ToUpper()}!");
-} 
+// foreach( var number in numbers){
+//     Console.WriteLine($"(number)");
+// }
