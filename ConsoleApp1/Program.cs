7a8201187017daaded2956603d768ef9112a894a
Abdullah Usman <iamabdullahusman837@gmail.com>
2024-01-09 11:18:11 +0500

    OOP concepts in OOP

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,15 @@
-﻿// specify the data source 
-int [] scores = [10,20,30,40];
-// Define the query expression
-IEnumerable<int> scoreQuery = 
- from score in scores
- where score > 80
- select score;
- // Excecute the query
- foreach(var i in scoreQuery)
-{
-    Console.Write(i + " ");
-}
+﻿Console.WriteLine(" Concept of OOP in C Sharp");
 
+var p1 = new Person ("Abdullah", "usman", new DateOnly (2000,5,1));
+var p2 = new Person ("Abdullah", "usman", new DateOnly (2000,5,1));
+List<Person> people = [p1,p2];
+Console.WriteLine(people.Count); 
+public class Person(string firstname, string lastname, DateOnly birthday){
+    
+    public string First {get ;} = firstname;
+    public string Last {get ;} =lastname;
+    public DateOnly Birthday {get ;} =  birthday;
+
+}
 
 
